1cb005c4ff0c5997be22cc2a1afd9215725629c9
j-mendez <jeff@a11ywatch.com>
2024-07-02 15:36:22 -0400

    ci(test): fix supabase auto-refresh hang

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ def read_file(fname):
 
 setup(
     name="spider-client",
-    version="0.0.40",
+    version="0.0.43",
     url="https://github.com/spider-rs/spider-clients/tree/main/python",
     author="Spider",
     author_email="jeff@a11ywatch.com",
