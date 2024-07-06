ffe2f7f6ba0ea3922332e191d56dbfa49c21f124
j-mendez <jeff@a11ywatch.com>
2024-07-06 07:45:10 -0400

    release(minor): 0.0.51

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ def read_file(fname):
 
 setup(
     name="spider-client",
-    version="0.0.49",
+    version="0.0.51",
     url="https://github.com/spider-rs/spider-clients/tree/main/python",
     author="Spider",
     author_email="jeff@a11ywatch.com",
